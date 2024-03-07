0e1ff888d22d4f699379bfac75dd7f0f717673a3
begana choi <begana.contact@gmail.com>
2024-03-07 15:20:34 +0100

    add comment

--- script.js
+++ script.js
@@ -5,14 +5,17 @@ console.log(`this is test`);
 //create a program that determines whether a given number is even or odd:
 
 // collect user input
-const input = prompt(`give me a number to figure out it's even or odd!`);
+// const input = prompt(`give me a number to figure out it's even or odd!`);
 
 // number or string?
 const convertToNum = Number(input);
 
+// declare a funtion
 function isOddOrEven(num) {
+  // if string, return invalid number
   if (Number.isNaN(num)) {
     console.log(`invalid input - please type a number.`);
+    // if number, return even or odd
   } else {
     if (num % 2 === 0) {
       console.log(`${num} is even!`);
@@ -22,7 +25,5 @@ function isOddOrEven(num) {
   }
 }
 
+// call the function
 isOddOrEven(convertToNum);
-
-// if number, return even or odd
-// if string, return invalid number
