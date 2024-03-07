3372c6c17a6f61490c6044ed755db6e06c8afc5c
begana choi <begana.contact@gmail.com>
2024-03-07 15:14:04 +0100

    even odd game (finished test(

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
+"use strict";
+
 console.log(`this is test`);
 
 //create a program that determines whether a given number is even or odd:
-const num = prompt(`give me a number to figure out it's even or odd!`);
 
-function evenOrOdd(num) {
-  if (num % 2 === 0) {
-    return console.log(`${num} is even`);
+// collect user input
+const input = prompt(`give me a number to figure out it's even or odd!`);
+
+// number or string?
+const convertToNum = Number(input);
+
+function isOddOrEven(num) {
+  if (Number.isNaN(num)) {
+    console.log(`invalid input - please type a number.`);
   } else {
-    return console.log(`${num} is odd`);
+    if (num % 2 === 0) {
+      console.log(`${num} is even!`);
+    } else {
+      console.log(`${num} is odd!`);
+    }
   }
 }
 
-evenOrOdd(num);
+isOddOrEven(convertToNum);
+
+// if number, return even or odd
+// if string, return invalid number
