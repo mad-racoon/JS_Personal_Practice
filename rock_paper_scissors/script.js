87357f3885ff7f74dbc594f866335884a5471dbf
begana choi <begana.contact@gmail.com>
2024-04-05 21:42:31 +0200

    starting point

--- rock_paper_scissors/script.js
+++ rock_paper_scissors/script.js
@@ -1,1 +1,7 @@
 "use strict";
+
+// Starting point
+const messageEl = document.querySelector(".message");
+const playerImg = document.querySelector(".img--player");
+messageEl.textContent = "make your decision";
+playerImg.src = "rock-paper-scissors.png";
