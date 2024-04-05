bc971093dec73d47e817ad1eb33dbcd6371c22e9
begana choi <begana.contact@gmail.com>
2024-04-05 21:50:35 +0200

    create basic events

--- rock_paper_scissors/script.js
+++ rock_paper_scissors/script.js
@@ -1,7 +1,25 @@
 "use strict";
 
-// Starting point
+// Selecting elements
 const messageEl = document.querySelector(".message");
 const playerImg = document.querySelector(".img--player");
+const btnRock = document.querySelector(".btn--rock");
+const btnScissors = document.querySelector(".btn--scissors");
+const btnPaper = document.querySelector(".btn--paper");
+
+// Starting point
 messageEl.textContent = "make your decision";
 playerImg.src = "rock-paper-scissors.png";
+
+// Making decision
+btnRock.addEventListener("click", function () {
+  playerImg.src = "rock.png";
+});
+
+btnScissors.addEventListener("click", function () {
+  playerImg.src = "scissors.png";
+});
+
+btnPaper.addEventListener("click", function () {
+  playerImg.src = "paper.png";
+});
